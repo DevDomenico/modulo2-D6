a79ffe5652d298b2b3e450900a36b4a1f1319da0
Domenico <135279072+DevDomenico@users.noreply.github.com>
2023-07-09 11:59:58 +0200

    esercizio

--- index.js
+++ index.js
@@ -1,22 +1,51 @@
-// Il tuo compito è creare un sito e-commerce per Smartphones. 
-// L'homepage che stai per creare sarà la vetrina dei prodotti disponibili, con qualche informazione per ciascun prodotto. 
-// Non sono richieste funzionalità di carrello/cassa.
-// - Completa gli esercizi dall'1 al 6 SOLAMENTE con HTML
-// - Completa gli altri esercizi con JS
-
-//ESERCIZI IN HTML
-
-// ESERCIZIO 1: Inserisci un tag h1 con il nome del tuo negozio
-// ESERCIZIO 2: Aggiungi una tabella con i 5 prodotti più in vista del tuo negozio
-// ESERCIZIO 3: Aggiungi per ogni prodotto un'immagine, un titolo, una descrizione e un prezzo
-// ESERCIZIO 4: Per ogni elemento della tabella aggiungi un link di Amazon al prodotto esistente
-// ESERCIZIO 5: Aggiungi un footer con il nome e l'indirizzo del tuo negozio
-// ESERCIZIO 6: Aggiungi un campo testuale in cui l'utente può lasciare un commento su un prodotto (al momento non serve inserire nessuna "vera" funzionalità di POST/salvataggio!)
 
 //ESERCIZI IN JS
-// ESERCIZIO 7: Scrivi una funzione per cambiare il contenuto del tag h1 in qualcos'altro
-// ESERCIZIO 8: Scrivi una funzione per cambiare il colore di background della pagina
-// ESERCIZIO 9: Scrivi una funzione per cambiare l'indirizzo presente nel futuro in un altro, fittizio
-// ESERCIZIO 10: Scrivi una funzione per aggiungere una classe CSS ad ogni link Amazon della tabella
+
+
+
+
 // ESERCIZIO 11: Scrivi una funzione per aggiungere/togliere una classe CSS a tutte le immagini della tabella; questa classe deve modificare la visibilità/invisibilità dell'immagine
 // ESERCIZIO 12: Scrivi una funzione per cambiare il colore del prezzo di ogni prodotto in uno differente, ogni volta che viene invocata
+
+
+// ESERCIZIO 7: Scrivi una funzione per cambiare il contenuto del tag h1 in qualcos'altro
+
+function modificaTitolo(titolo){
+    let tagtitolo =  document.querySelector("h1")
+
+    tagtitolo.innerText = titolo;
+       
+}
+ //modificaTitolo("Sito ecommerce")
+
+
+// ESERCIZIO 8: Scrivi una funzione per cambiare il colore di background della pagina
+
+function modificaColorePagina(){
+    let body = document.querySelector("body");
+   body.style.background = "red";
+}
+// modificaColorePagina() 
+
+// ESERCIZIO 9: Scrivi una funzione per cambiare l'indirizzo presente nel futuro in un altro, fittizio
+
+//utilizzando un id al P del footer modifico solo l'indirizzo
+function modificaIndirizzo() {
+
+    let footer = document.querySelector("#indirizzo");
+    footer.innerText = "via nazionale 12,Roma"
+}
+modificaIndirizzo();
+
+// ESERCIZIO 10: Scrivi una funzione per aggiungere una classe CSS ad ogni link Amazon della tabella
+
+// ho utilizzato un loop 
+function modificaClasse (){
+    let tabelle = document.querySelectorAll("td")
+    for ( i=0; i < tabelle.length; i++){
+        tabelle[i].classList.add("colore")
+    }
+}
+//modificaClasse();
+
+// ESERCIZIO 11: Scrivi una funzione per aggiungere/togliere una classe CSS a tutte le immagini della tabella; questa classe deve modificare la visibilità/invisibilità dell'immagine
